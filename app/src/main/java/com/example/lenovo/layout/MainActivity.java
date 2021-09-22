314401f04ee8b1551b5625d79792d564ea24f77b
tux <rubenoctavio@itsn.edu.mx>
2021-09-22 03:54:33 -0500

    se agregaron todos los archivos al repositorio

---
+++ app/src/main/java/com/example/lenovo/layout/MainActivity.java
@@ -0,0 +1,13 @@
+package com.example.lenovo.layout;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.frame);
+    }
+}
